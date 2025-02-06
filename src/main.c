1bd328f694ba0eea85b1c0d64d34f72033843055
Jorge <jorgetrujilloroman@gmail.com>
2025-02-06 17:09:27 +0100

    todo: serve table

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jotrujil <jotrujil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 13:45:26 by jotrujil          #+#    #+#             */
-/*   Updated: 2025/02/06 14:10:23 by jotrujil         ###   ########.fr       */
+/*   Updated: 2025/02/06 14:16:59 by jotrujil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,5 @@ int	main(int argc, char **argv)
 	if (args_check(argc, argv))
 		return (1);
 	table = serve_table(table);
+	return (0);
 }
